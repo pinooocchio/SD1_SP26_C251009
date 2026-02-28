0d7e850f43e8941c0e8d32eb1475890534ac9345
pinocchio <c251009@ugrad.iiuc.ac.bd>
2026-02-28 22:41:46 +0600

    Initial commit

---
+++ plm1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class plm1 {
+    public static void main(String[] arg)
+
+    {
+        System.out.print("Hello,World");
+    }
+}
---
+++ plm2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class plm2 {
+    public static void main(String[] arg)
+    {
+        Scanner input = new Scanner( System.in);
+        String str = input.next();
+        System.out.println(str);  
+    }
+}
