b22acb6dc82b5b7e8fae53ed466a143b82545006
pinocchio <c251009@ugrad.iiuc.ac.bd>
2026-03-05 16:06:40 +0600

    adding new problems

---
+++ Java Codes/plm10.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class plm10 {
+    public static void main(String[] arg) {
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+}
